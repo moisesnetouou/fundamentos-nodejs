58a5a47e860eacc56b4fda49dc25a2f6b5e0cc64
Moisés <moisesnetored@gmail.com>
2021-11-30 11:25:36 -0400

    feat: Utilizando metodos HTTP com o Insominia

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 const express = require('express'); 
 
-const app = express();  
+const app = express(); 
 
-app.get("/", (request, response)=> {
-  return response.json({message: "Hello World Ignite - Fundamentos NodeJS!"})
+/**
+ * GET    - Buscar uma informação dentro do nosso servidor
+ * POST   - Inserir uma informação no servidor.
+ * PUT    - Alterar uma informação no servidor.
+ * PATCH  - Alterar uma informação especifica
+ * DELETE - Deletar uma informação no servidor.
+ */
+
+app.get("/courses", (request, response)=> {
+  return response.json([
+    "Curso 1", "Curso 2", "Curso 3"
+  ])
+})
+
+app.post("/courses", (request, response) => {
+  return response.json([
+    "Curso 1", "Curso 2", "Curso 3", "Curso 4"
+  ])
+})
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6", "Curso 2", "Curso 3", "Curso 4"
+  ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6", "Curso 7", "Curso 3", "Curso 4"
+  ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6", "Curso 2",  "Curso 4"
+  ])
 })
 
 app.listen(3333); 
