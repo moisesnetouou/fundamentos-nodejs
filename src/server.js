854c5a94ab8966b8b5b1d48dcf16fe7f694cbfe0
Moisés Neto <moisesnetored@gmail.com>
2023-03-11 13:12:27 -0400

    feat: about headers and status code

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import http from "node:http"
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const {method, url} = req
 
   if(method === 'GET' && url === "/users") {
-    return res.end("Listagem de usuários")
+    return res
+    .setHeader("Content-type", "application/json")
+    .end(JSON.stringify(users))
   }
 
   if(method === "POST" && url === "/users") {
-    return res.end("Criação de usuário")
+    users.push({
+      id: 1,
+      name: "Moisés",
+      email: "moises@example.com"
+    })
+
+    return res.writeHead(201).end("Criação de usuário")
   }
   
-  return res.end("Hello World")
+  return res.writeHead(404).end("Not found")
 })
 
 server.listen(3333)
