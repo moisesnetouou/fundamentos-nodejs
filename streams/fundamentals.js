7498eadd904886f16e7bc60f9afe73ed7fa47d80
Moisés Neto <moisesnetored@gmail.com>
2023-03-11 13:52:19 -0400

    feat: stream readable, writable and transform

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream"
+import { Readable, Transform, Writable } from "node:stream"
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -18,4 +18,26 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class MultiplyByTenStreams extends Writable {
+  _write(chunk, enconding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString() * -1)
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+// new OneToHundredStream().pipe(process.stdout)
+
+/* new OneToHundredStream()
+  .pipe(new MultiplyByTenStreams()) */
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStreams())
