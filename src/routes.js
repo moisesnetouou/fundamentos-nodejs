517a0502cbf74d86836322de45e6aed4dafc6134
Moisés <moisesnetored@gmail.com>
2023-06-12 15:02:43 -0400

    feat: routes with parameters (reg ex)

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { randomUUID } from "node:crypto"
 
 import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: 'GET',
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const {name, email} = req.body
 
@@ -29,6 +30,18 @@ export const routes = [
       database.insert('users', user)
 
       return res.writeHead(201).end("Criação de usuário")
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      // const { id } = req.params
+
+      // database.delete('users', id)
+
+      // return res.writeHead(204).end()
+      return res.end()
     }
   }
 ]
